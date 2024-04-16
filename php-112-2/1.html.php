3bc6ee30e1e8d715a4ef9a2baaf207a236224dd0
Hectooooooor <155548369+Hectooooooor@users.noreply.github.com>
2024-04-16 19:26:30 +0800

    Update 1.html.php

--- php-112-2/1.html.php
+++ php-112-2/1.html.php
@@ -3,8 +3,8 @@
     <body>
         <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
         <h1>html</h1>
-        <?php echo "Hello world!";  ?> // 再次輸出 "Hello world!" 字串
+        <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
         <h1>html</h1>
-        <?php echo "Hello world!";  ?> // 一次又一次輸出 "Hello world!" 字串
+        <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
     </body>
 </html>
