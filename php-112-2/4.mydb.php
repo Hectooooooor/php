609720fe4a5aafcaefa7de4b8ad3fae7814a00fc
Hectooooooor <155548369+Hectooooooor@users.noreply.github.com>
2024-04-16 19:42:45 +0800

    Update 4.mydb.php

--- php-112-2/4.mydb.php
+++ php-112-2/4.mydb.php
@@ -5,7 +5,10 @@
     $result=mysqli_query($conn, "select * from user");
     #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
     $row=mysqli_fetch_array($result);
-    echo $row["id"] . " " . $row["pwd"]."<br>"; 
-    $row=mysqli_fetch_array($result);
+    // 輸出第一行使用者的帳號和密碼
+    echo $row["id"] . " " . $row["pwd"] . "<br>";    
+    // 從查詢結果中再次取出一行資料
+    $row = mysqli_fetch_array($result);    
+    // 輸出第二行使用者的帳號和密碼
     echo $row["id"] . " " . $row["pwd"];
 ?>
