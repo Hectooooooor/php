8d0c8ec86abc9b2ce892862ac36e6d353fecea8b
Hectooooooor <155548369+Hectooooooor@users.noreply.github.com>
2024-04-16 19:44:49 +0800

    Update 6.login.php

--- php-112-2/6.login.php
+++ php-112-2/6.login.php
@@ -5,13 +5,18 @@
    $result=mysqli_query($conn, "select * from user");
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    $login=FALSE;
+   // 逐行從查詢結果中取出使用者的資料並進行驗證
    while ($row=mysqli_fetch_array($result)) {
+     // 檢查用戶提交的帳號和密碼是否與資料庫中的任何記錄匹配
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
+       // 如果匹配成功，將登入狀態設置為 TRUE
        $login=TRUE;
      }
    } 
+   
+   // 根據登入狀態輸出不同的訊息
    if ($login==TRUE)
      echo "登入成功";
-  else
+   else
      echo "帳號/密碼 錯誤";
 ?>
